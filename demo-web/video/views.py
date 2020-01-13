bf1516b33def4d916e5f766d6b06cf04b34547da
Kim Ayeong <rladkdud31@naver.com>
2020-01-13 23:31:48 +0900

    Add webcam.py

--- demo-web/video/views.py
+++ demo-web/video/views.py
@@ -5,14 +5,3 @@ from .forms import UploadFileForm
 
 def base(request):
     return render(request, 'video/video.html', {})
-
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = UploadFileForm()
-    return render(request, 'video/video.html', {'form': form})
