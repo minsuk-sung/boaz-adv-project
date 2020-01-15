ec817bbce413d0625cf23563472e39ea03cc1892
Seogeurim <geulims@naver.com>
2020-01-15 23:45:17 +0900

    modify app.py

--- demo-web/video/views.py
+++ demo-web/video/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import UploadFileForm
-from .apps import VideoConfig
+import os
+from pathlib import Path
+
+from video.violence_model.predict import ViolencePredictor
 
 
 def base(request):
@@ -9,7 +12,14 @@ def base(request):
 
 
 def predict(request):
-    VideoConfig.predictor.predict()
+    BASE = os.path.dirname(os.path.abspath(__file__))
+    MODEL_PATH = Path(BASE, 'violence_model/trained/violence_resnet.model')
+    LABEL_PATH = Path(BASE, 'violence_model/trained/lb.pickle')
+    INPUT_PATH = os.path.normpath(Path(BASE, '../media/Riot.mp4'))
+    OUTPUT_PATH = os.path.normpath(Path(BASE, '../media/output/test1.mp4'))
+
+    ViolencePredictor(model_path=MODEL_PATH, label_path=LABEL_PATH,
+                      input_path=INPUT_PATH, output_path=OUTPUT_PATH, size=64)
     return render(request, 'video/base.html', {})
 
 
